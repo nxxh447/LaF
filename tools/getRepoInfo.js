9eca34749d9ea4f387721f59dd241aa182eb1b6f
nxxh447 <65509800+nxxh447@users.noreply.github.com>
2022-05-16 12:54:34 -0400

    [misc] formatting, single -> double quotes
    - some formatting changes
    - switched all single quotes to double quotes

--- tools/getRepoInfo.js
+++ tools/getRepoInfo.js
@@ -1,13 +1,10 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-fetch('https://api.github.com/repos/Hiro527/LaF/releases')
-.then(res => res.json())
-.then((data) => {
-    const latestRelease = data[0];
-    let downloads = 0;
-    Object.values(latestRelease.assets).forEach((v) => {
-        downloads += v.download_count;
-    });
-    console.log(`Downloads: ${downloads}`);
-})
-.catch(err => console.error);
+fetch("https://api.github.com/repos/Hiro527/LaF/releases")
+	.then((res) => res.json())
+	.then((data) => {
+    	let downloads = 0;
+    	Object.values(data[0].assets).forEach((v) => {
+        	downloads += v.download_count;
+    	});cconsole.log(`Downloads: ${downloads}`);
+	}).catch((err) => console.error);
